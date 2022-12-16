8f17aa1aba94c4ac73a06339d551be5d28312ecf
Шахрух <sahruhabd@MacBook-Air-Sahruh.local>
2022-12-16 19:07:14 +0600

    get post

--- my-app/src/components/main.js
+++ my-app/src/components/main.js
@@ -1,34 +1,35 @@
-
+import React, { useState, useEffect } from 'react';
 
 function Main() {
-
-    // const items = [];
-
-    // fetch('https://fakestoreapi.com/products/1')
-    //         .then(res=>res.json())
-    //         .then(
-    //             (result) => {
-    //             this.setState({
-    //                 isLoaded: true,
-    //                 items: result.prodcuts
-    //             });
-    //         },
-    //         (error) => {
-    //             this.setState({
-    //                 isLoaded: true,
-    //                 error
-    //             })
-    //         }
-    //     )
-
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products/category/electronics')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setPosts(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+    
     return(
-        <div>
-            {/* {items.map(item => (
-                key={item.name}
-                {item.title}
-                ))} */}
-        </div>
-    )
+        <div className="posts-container">
+      {posts.map((post) => {
+         return (
+            <>
+            {/* <div className="post-card" key={post.id}> */}
+               <h2 className="post-title">{post.title}</h2>
+               <h3>{post.price}</h3>
+               <img width={150} src={post.image} alt="" />
+                
+            {/* </div> */}
+            </>
+         );
+      })}
+   </div>
+   );
 }
 
 export default Main
